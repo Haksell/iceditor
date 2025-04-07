879803ee038d710897783415b14c458b27bc68c7
Haksell <axel.brisse@gmail.com>
2025-04-08 00:51:32 +0200

    editable

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,44 @@
-use iced::{Element, Sandbox, Settings, widget::text};
+use iced::{
+    Element, Sandbox, Settings,
+    widget::{container, text_editor},
+};
 
 fn main() -> iced::Result {
     Editor::run(Settings::default())
 }
 
-struct Editor;
+struct Editor {
+    content: text_editor::Content,
+}
 
-#[derive(Debug)]
-enum Message {}
+#[derive(Debug, Clone)]
+enum Message {
+    Edit(text_editor::Action),
+}
 
 impl Sandbox for Editor {
     type Message = Message;
 
     fn new() -> Self {
-        Self
+        Self {
+            content: text_editor::Content::new(),
+        }
     }
 
     fn title(&self) -> String {
-        String::from("A cool editor")
+        String::from("A cool editor!")
     }
 
     fn update(&mut self, message: Message) {
-        match message {}
+        match message {
+            Message::Edit(action) => {
+                self.content.edit(action);
+            }
+        }
     }
 
     fn view(&self) -> Element<'_, Message> {
-        text("Hello, iced!").into()
+        let input = text_editor(&self.content).on_edit(Message::Edit);
+        container(input).padding(10).into()
     }
 }
